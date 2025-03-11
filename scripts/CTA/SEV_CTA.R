cb0c20fe212619db5734eb78d39ea192cc135e4e
James Sturges <83841694+jwsturges1@users.noreply.github.com>
2025-03-10 22:27:34 -0400

    SEV CTA initial analysis script completed

--- scripts/CTA/SEV_CTA.R
+++ scripts/CTA/SEV_CTA.R
@@ -39,23 +39,28 @@ write.csv(species_n_producer_site, "tables/SEV/summary/species_n_producer_site.c
 # remove 4 sites with less than 10 years of data
 unique(sev_consumers$plot)
 unique(sev_producers$plot)
-site_order = c("1","2" ,"3", "4", "5")
 
 sev_consumers = sev_consumers %>% 
-  filter(!plot %in% c("004d", "004g", "010d", "n00b", "000b")) %>%
-  mutate(plot = factor(plot, levels = c("001d","004b" ,"n01b", "n04d", "002d", "004f", "002c", "0sub","020b", "0spb", "n20a", "n20b", "n01a", "n04a")))
+  mutate(plot = as_factor(plot))
 
-sev_producers = sev_producers %>% 
-  filter(!plot %in% c("004d", "004g", "010d", "n00b", "000b")) %>%
-  mutate(plot = factor(plot, levels = c("001d","004b" ,"n01b", "n04d", "002d", "004f", "002c", "0sub","020b", "0spb", "n20a", "n20b", "n01a", "n04a")))
+site_order_consumer = c("1","2" ,"3", "4", "5")
+site_order_producer = c("DW","FP")
+
+# sev_consumers = sev_consumers %>% 
+#   filter(!plot %in% c("004d", "004g", "010d", "n00b", "000b")) %>%
+#   mutate(plot = factor(plot, levels = c("001d","004b" ,"n01b", "n04d", "002d", "004f", "002c", "0sub","020b", "0spb", "n20a", "n20b", "n01a", "n04a")))
+# 
+# sev_producers = sev_producers %>% 
+#   filter(!plot %in% c("004d", "004g", "010d", "n00b", "000b")) %>%
+#   mutate(plot = factor(plot, levels = c("001d","004b" ,"n01b", "n04d", "002d", "004f", "002c", "0sub","020b", "0spb", "n20a", "n20b", "n01a", "n04a")))
 
 #only keep 15 producer sites that match consumer data
-sev_producers <- sev_producers %>%
-  filter(plot == site_order)
-
-sev_producers <- sev_producers %>%
-  mutate(plot = factor(plot, levels = site_order)) %>%  # Ensure plot matches site_order
-  arrange(match(plot, site_order))
+# sev_producers <- sev_producers %>%
+#   filter(plot == site_order)
+# 
+# sev_producers <- sev_producers %>%
+#   mutate(plot = factor(plot, levels = site_order)) %>%  # Ensure plot matches site_order
+#   arrange(match(plot, site_order))
 
 # Survey History Summary Tables ----
 
@@ -171,9 +176,7 @@ plot_nmds_consumer <- ggplot(sev_consumer_means, aes(x = MDS1,
   geom_point(size = 3) +
   geom_text(check_overlap = TRUE, size = 3, color = "black", vjust = 1.5) +
   labs(title = "SEV Consumer Community by Site", x = "NMDS1", y = "NMDS2") +
-  scale_color_manual(values = c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-                                "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
-                                "#aec7e8", "#ffbb78", "#98df8a", "#ff9896")) +
+  scale_color_manual(values = c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd")) +
   # scale_shape_manual(values = c(17,15,18,19)) +
   guides(color = guide_legend(title = "Site")) +
   theme_classic()
@@ -211,9 +214,7 @@ ggsave(filename = paste0("figures/NMDS/SEV", "/", "plot_nmds_producer", ".png"),
 D <- vegan::vegdist(sev_consumer_matrix,"bray")
 
 custom_palette <- c(
-  "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-  "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
-  "#aec7e8", "#ffbb78", "#98df8a", "#ff9896"
+  "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"
 ) 
 
 # Generate the plot using trajectoryPCoA
@@ -223,9 +224,7 @@ sev_consumer_means = sev_consumer_means %>%
 
 
 trajectoryPCoA(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums, traj.colors = custom_palette, lwd = 1, survey.labels = T) 
-legend(ncol = 1, y.intersp = 0.5, "topright", inset = c(0.02, 0), bty="n", legend=c("001d", "004b", "n01b", "n04d", "002d", "004f", "002c", "0sub", "020b", "0spb", "n20a", "n20b", "n01a", "n04a"),  col = c( "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-                                                                                                                                                                                                               "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
-                                                                                                                                                                                                               "#aec7e8", "#ffbb78", "#98df8a", "#ff9896"), lwd=2)
+legend(ncol = 1, y.intersp = 0.5, "topright", inset = c(0.02, 0), bty="n", legend=c("1","2","3","4","5"),  col = c( "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"), lwd=2)
 
 
 # Capture the plot output
@@ -243,9 +242,7 @@ sev_producer_means = sev_producer_means %>%
   mutate(vector_nums = year - min(year) + 1)
 
 trajectoryPCoA(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums, traj.colors = custom_palette, lwd = 2, survey.labels = T) 
-legend(ncol = 1, y.intersp = 0.5, "topright", inset = c(0.02, 0), bty="n", legend=c("001d", "004b", "n01b", "n04d", "002d", "004f", "002c", "0sub", "020b", "0spb", "n20a", "n20b", "n01a", "n04a"),  col = c( "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-                                                                                                                                                                                                               "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
-                                                                                                                                                                                                               "#aec7e8", "#ffbb78", "#98df8a", "#ff9896"), lwd=2)
+legend(ncol = 1, y.intersp = 0.5, "topright", inset = c(0.02, 0), bty="n", legend=c("DW", "FP"),  col = c( "#1f77b4", "#ff7f0e"), lwd=2)
 
 # Capture the plot output
 trajectory_plot <- recordPlot()
@@ -265,9 +262,7 @@ unique_sites <- unique(sev_consumers$plot)
 # Loop through each site
 # Define a vector of colors (add more as needed)
 color_vector <- c(
-  "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-  "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
-  "#aec7e8", "#ffbb78", "#98df8a", "#ff9896"
+  "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd"
 )   # Extend this list as more sites are added
 
 for (site in unique_sites) {
@@ -315,14 +310,12 @@ for (site in unique_sites) {
 
 
 #producers
-unique_sites_producers <- unique(sev_consumers$plot)
+unique_sites_producers <- unique(sev_producers$plot)
 
 # Loop through each site
 # Define a vector of colors (add more as needed)
 color_vector <- c(
-  "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-  "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
-  "#aec7e8", "#ffbb78", "#98df8a", "#ff9896"
+  "#1f77b4", "#ff7f0e"
 )   # Extend this list as more sites are added
 
 for (site in unique_sites_producers) {
@@ -375,84 +368,84 @@ for (site in unique_sites_producers) {
 # Calculate trajectory lengths (not relative to initial)
 consumer_lengths <- trajectoryLengths(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums)
 consumer_lengths <- as.data.frame(consumer_lengths)
-consumer_lengths$site <- site_order  
+consumer_lengths$site <- site_order_consumer  
 write.csv(consumer_lengths, "tables/SEV/consumer/consumer_lengths.csv", row.names = FALSE)
 
 # Calculate trajectory lengths (relative to initial)
 consumer_lengths_initial <- trajectoryLengths(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums, relativeToInitial = TRUE)
 consumer_lengths_initial <- as.data.frame(consumer_lengths_initial)
-consumer_lengths_initial$site <- site_order
+consumer_lengths_initial$site <- site_order_consumer
 write.csv(consumer_lengths_initial, "tables/SEV/consumer/consumer_lengths_initial.csv", row.names = FALSE)
 
 # Calculate trajectory angles (not relative to initial)
 consumer_angles <- trajectoryAngles(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums)
 consumer_angles <- as.data.frame(consumer_angles)
-consumer_angles$site <- site_order
+consumer_angles$site <- site_order_consumer
 write.csv(consumer_angles, "tables/SEV/consumer/consumer_angles.csv", row.names = FALSE)
 
 # Calculate trajectory angles (relative to initial)
 consumer_angles_initial <- trajectoryAngles(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums, relativeToInitial = TRUE)
 consumer_angles_initial <- as.data.frame(consumer_angles_initial)
-consumer_angles_initial$site <- site_order
+consumer_angles_initial$site <- site_order_consumer
 write.csv(consumer_angles_initial, "tables/SEV/consumer/consumer_angles_initial.csv", row.names = FALSE)
 
 # # Calculate trajectory distances
 # consumer_trajectory_distances <- trajectoryDistances(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums)
 # consumer_trajectory_distances <- as.data.frame(consumer_trajectory_distances)
-# consumer_trajectory_distances$site <- site_order
+# consumer_trajectory_distances$site <- site_order_consumer
 # write.csv(consumer_trajectory_distances, "tables/SEV/consumer/consumer_trajectory_distances.csv", row.names = FALSE)
 
 # Calculate trajectory convergence
 consumer_convergence <- trajectoryConvergence(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums)
 consumer_convergence <- as.data.frame(consumer_convergence)
-consumer_convergence$site <- site_order
+consumer_convergence$site <- site_order_consumer
 write.csv(consumer_convergence, "tables/SEV/consumer/consumer_convergence.csv", row.names = FALSE)
 
 # Calculate trajectory directionality
 consumer_directionality <- trajectoryDirectionality(D, sites = sev_consumer_means$plot, surveys = sev_consumer_means$vector_nums)
 consumer_directionality <- as.data.frame(consumer_directionality)
-consumer_directionality$site <- site_order
+consumer_directionality$site <- site_order_consumer
 write.csv(consumer_directionality, "tables/SEV/consumer/consumer_directionality.csv", row.names = FALSE)
 
 # Extract trajectory metrics for producers
 # Calculate trajectory lengths (not relative to initial)
 producer_lengths <- trajectoryLengths(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums)
 producer_lengths <- as.data.frame(producer_lengths)
-producer_lengths$site <- site_order
+producer_lengths$site <- site_order_producer
 write.csv(producer_lengths, "tables/SEV/producer/producer_lengths.csv", row.names = FALSE)
 
 # Calculate trajectory lengths (relative to initial)
 producer_lengths_initial <- trajectoryLengths(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums, relativeToInitial = TRUE)
 producer_lengths_initial <- as.data.frame(producer_lengths_initial)
-producer_lengths_initial$site <- site_order
+producer_lengths_initial$site <- site_order_producer
 write.csv(producer_lengths_initial, "tables/SEV/producer/producer_lengths_initial.csv", row.names = FALSE)
 
 # Calculate trajectory angles (not relative to initial)
 producer_angles <- trajectoryAngles(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums)
 producer_angles <- as.data.frame(producer_angles)
-producer_angles$site <- site_order
+producer_angles$site <- site_order_producer
 write.csv(producer_angles, "tables/SEV/producer/producer_angles.csv", row.names = FALSE)
 
 # Calculate trajectory angles (relative to initial)
 producer_angles_initial <- trajectoryAngles(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums, relativeToInitial = TRUE)
 producer_angles_initial <- as.data.frame(producer_angles_initial)
-producer_angles_initial$site <- site_order
+producer_angles_initial$site <- site_order_producer
 write.csv(producer_angles_initial, "tables/SEV/producer/producer_angles_initial.csv", row.names = FALSE)
 
 # # Calculate trajectory distances
 # producer_trajectory_distances <- trajectoryDistances(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums)
 # producer_trajectory_distances <- as.data.frame(producer_trajectory_distances)
-# producer_trajectory_distances$site <- site_order
+# producer_trajectory_distances$site <- site_order_producer
 # write.csv(producer_trajectory_distances, "tables/SEV/producer/producer_trajectory_distances.csv", row.names = FALSE)
 
 # Calculate trajectory convergence
 producer_convergence <- trajectoryConvergence(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums)
 producer_convergence <- as.data.frame(producer_convergence)
-producer_convergence$site <- site_order
+producer_convergence$site <- site_order_producer
 write.csv(producer_convergence, "tables/SEV/producer/producer_convergence.csv", row.names = FALSE)
 
 # Calculate trajectory directionality
 producer_directionality <- trajectoryDirectionality(Dd, sites = sev_producer_means$plot, surveys = sev_producer_means$vector_nums)
 producer_directionality <- as.data.frame(producer_directionality)
-producer_directionality$site <- site_order
+producer_directionality$site <- site_order_producer
 write.csv(producer_directionality, "tables/SEV/producer/producer_directionality.csv", row.names = FALSE)
