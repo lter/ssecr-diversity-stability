411737c3b4e228f9118f980eacfeae091a821463
Julianna <julianna.renzi@gmail.com>
2025-03-13 17:04:46 -0700

    Worked more on PISCO but got stuck with abundances vs. percent cover/other Q's--emailing Jenn

--- scripts/wrangling_julianna/wrangling_julianna_pisco.R
+++ scripts/wrangling_julianna/wrangling_julianna_pisco.R
@@ -34,3 +34,47 @@ librarian::shelf(here, # relative file paths
 #             Load data ----
 ## -------------------------------------------- ##
 
+fish <- read_csv(here("..", "data", "PISCO", "PISCO_kelpforest_fish.1.4.csv"))
+swath <- read_csv(here("..", "data", "PISCO", "PISCO_kelpforest_swath.1.3.csv"))
+upc <- read_csv(here("..", "data", "PISCO", "PISCO_kelpforest_upc.1.3.csv"))
+
+## -------------------------------------------- ##
+#           Temporal scope of data ----
+## -------------------------------------------- ##
+
+# are survey_year and year the same?
+unique(fish$survey_year == fish$year) # yes!
+
+# get temporal distribution of sites/sampling
+fish %>% 
+  group_by(year, site) %>% 
+  summarize(total_fish = sum(count)) %>% 
+  pivot_wider(names_from = year,
+              values_from = total_fish) %>% write_csv(here("..", "data", "pisco_site_survey_distribution.csv"))
+
+# manually annotated then bring it back in:
+temporal_span <- read_csv(here("..", "data", "pisco_site_survey_distribution_manual.csv"))
+
+# how many sites have at least 10 years continuous:
+temporal_span %>% 
+  filter(CONSECUTIVE_10YEARS == "y") %>% dim() # 89
+
+temporal_span %>% 
+  filter(CONSECUTIVE_10YEARS == "9") %>% dim() # 7
+
+## -------------------------------------------- ##
+#                   QA/QC ----
+## -------------------------------------------- ##
+
+# seems like there can be different methods in the same year/site?
+# with method:
+fish %>% 
+  group_by(year, method, site, zone, transect) %>% 
+  summarize(total_fish = sum(count)) %>% dim() # more than the next
+
+# without:
+fish %>% 
+  group_by(year, site, zone, transect) %>% 
+  summarize(total_fish = sum(count)) %>% dim() # 1,180 fewer
+
+# there are missing years within the time series
