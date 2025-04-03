2d92262e460845d09242d47d07e9e15a79f4c0f4
Julianna <julianna.renzi@gmail.com>
2025-04-03 11:48:33 -0700

    Fixed typo in plot column

--- scripts/wrangling_julianna/wrangling_julianna_MCR_algae.R
+++ scripts/wrangling_julianna/wrangling_julianna_MCR_algae.R
@@ -155,7 +155,8 @@ algae_4 %>%
   rename(year = Year) %>% 
   mutate(month = month(Date), 
          day = day(Date)) %>% 
-  mutate(plot = str_to_lower(paste0(str_split(Site, pattern = " ")[[1]][1], "_", str_split(Site, pattern = " ")[[1]][2]))) %>% 
+  mutate(plot = str_to_lower(paste0(str_split(Site, pattern = "_")[[1]][1], "_", 
+                                    str_split(Site, pattern = "_")[[1]][2]))) %>% 
   mutate(subplot = paste0(Transect, "_", Quadrat)) %>% 
   mutate(unique_ID = paste0(site, "_", habitat_fine, "_", plot)) %>% 
   mutate(unit_abundance = "percent") %>% 
