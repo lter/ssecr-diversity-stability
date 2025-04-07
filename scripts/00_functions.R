a3c53be775f3b2a42b0331687d2568a7fa90a3f3
naltmank <nta8@gatech.edu>
2025-04-07 11:15:26 -0400

    moved aggregate stability function to this 00_functions script. Updated filter_data function to include option to minimize

--- scripts/00_functions.R
+++ scripts/00_functions.R
@@ -18,7 +18,12 @@ stability <- function(x){
 }
 
 #### filter_data() - function to streamline and clean datasets for synthesis ####
-filter_data <- function(site_name, producer_data, consumer_data, mean_sum, write_csv = FALSE) {
+filter_data <- function(site_name, # site name as string
+                        producer_data, # producer df
+                        consumer_data, # consumer df
+                        mean_sum, # c("mean", "sum") indicates whether plots should be averaged or summed when aggregating
+                        minimize = FALSE, # subset for shortest possible time series based on spatio-temporally co-located plots
+                        write_csv = FALSE) {
   print("Starting function execution...")
   
   # Aggregate data at the year-plot level
@@ -41,6 +46,7 @@ filter_data <- function(site_name, producer_data, consumer_data, mean_sum, write
       summarise(abundance = sum(abundance, na.rm = TRUE), .groups = "drop")
   }
   
+  # pivot data to wide format
   print("Pivoting data to wide format...")
   producer_wide <- producer_agg %>%
     pivot_wider(names_from = taxon_name, values_from = abundance, values_fill = list(abundance = 0))
@@ -48,6 +54,7 @@ filter_data <- function(site_name, producer_data, consumer_data, mean_sum, write
   consumer_wide <- consumer_agg %>%
     pivot_wider(names_from = taxon_name, values_from = abundance, values_fill = list(abundance = 0))
   
+  # subset timpoints and plots that sampled consumers and producers in the same place/year 
   print("Checking valid plot-year combinations...")
   valid_plot_years <- inner_join(producer_wide %>% select(plot, year),
                                  consumer_wide %>% select(plot, year),
@@ -62,6 +69,38 @@ filter_data <- function(site_name, producer_data, consumer_data, mean_sum, write
     inner_join(valid_plot_years, by = c("plot", "year")) %>%
     mutate(site = site_name)
   
+  # Minimize — remove plots with < 10 years of data and then standardize remaining plots by shortest time series possible
+  if (minimize) {
+    # Remove plots with <10 years of data
+    plot_year_counts <- producer_wide_sub %>%
+      group_by(plot) %>%
+      summarise(n_years = n_distinct(year), .groups = "drop") %>%
+      filter(n_years >= 10)
+    
+    valid_plots <- plot_year_counts$plot
+    
+    producer_wide_sub <- producer_wide_sub %>%
+      filter(plot %in% valid_plots)
+    
+    consumer_wide_sub <- consumer_wide_sub %>%
+      filter(plot %in% valid_plots)
+    
+    # Determine overlapping year range
+    year_ranges <- producer_wide_sub %>%
+      group_by(plot) %>%
+      summarise(min_year = min(year), max_year = max(year), .groups = "drop")
+    
+    min_range_start <- max(year_ranges$min_year)
+    max_range_end <- min(year_ranges$max_year)
+    
+    producer_wide_sub <- producer_wide_sub %>%
+      filter(year >= min_range_start, year <= max_range_end)
+    
+    consumer_wide_sub <- consumer_wide_sub %>%
+      filter(year >= min_range_start, year <= max_range_end)
+  }
+  
+  # assign created dataframes to global environment
   print("Creating final object names...")
   producer_object_name <- paste0(site_name, "_producers_wide_sub")
   consumer_object_name <- paste0(site_name, "_consumers_wide_sub")
@@ -70,6 +109,7 @@ filter_data <- function(site_name, producer_data, consumer_data, mean_sum, write
   assign(producer_object_name, producer_wide_sub, envir = .GlobalEnv)
   assign(consumer_object_name, consumer_wide_sub, envir = .GlobalEnv)
   
+  # write csv
   if (write_csv) {
     print("Writing to CSV...")
     write_csv(producer_wide_sub, paste0(producer_object_name, ".csv"))
@@ -92,6 +132,131 @@ filter_data <- function(site_name, producer_data, consumer_data, mean_sum, write
 # functions to extract ranges of data (from mcr algal working group github)
 # custom functions by Noam - could probably be made more efficient
 # function to extract the ranges of the data for plotting
+
+
+
+####  create aggregate diversity stability dataframe ####
+calculate_agg_stability <- function(producer_data, # synthesized producer data after filtering
+                                    consumer_data, # synthesized consumer data after filtering
+                                    ecosystem_type, # vector c("Terrestrial", "Marine") indictaing ecosystem type
+                                    write_csv = FALSE # option to automatically write csv
+) {
+  
+  # calculate diversity for producers and consumers
+  producer_diversity <- 
+    data.frame(
+      site = producer_data$site,
+      taxa_type = producer_data$taxa_type,
+      ecosystem = producer_data$ecosystem,
+      habitat_broad = producer_data$habitat_broad,
+      habitat_fine = producer_data$habitat_fine,
+      biome = producer_data$biome,
+      guild = producer_data$guild,
+      plot = producer_data$plot,
+      year = producer_data$year,
+      richness = rowSums(producer_data[, -which(names(producer_data) %in% long_meta_cols)] > 0),
+      abundance = rowSums(producer_data[, -which(names(producer_data) %in% long_meta_cols)]),
+      shannon = vegan::diversity(producer_data[, -which(names(producer_data) %in% long_meta_cols)], "shannon")
+    )
+  
+  consumer_diversity <- 
+    data.frame(
+      site = consumer_data$site,
+      taxa_type = consumer_data$taxa_type,
+      ecosystem = consumer_data$ecosystem,
+      habitat_broad = consumer_data$habitat_broad,
+      habitat_fine = consumer_data$habitat_fine,
+      biome = consumer_data$biome,
+      guild = consumer_data$guild,
+      plot = consumer_data$plot,
+      year = consumer_data$year,
+      richness = rowSums(consumer_data[, -which(names(consumer_data) %in% long_meta_cols)] > 0),
+      abundance = rowSums(consumer_data[, -which(names(consumer_data) %in% long_meta_cols)]),
+      shannon = vegan::diversity(consumer_data[, -which(names(consumer_data) %in% long_meta_cols)], "shannon")
+    )
+  
+  # create dss dataframes for producers and consumers
+  producer_dss <- producer_diversity %>%
+    dplyr::group_by(site, taxa_type, ecosystem, habitat_broad, habitat_fine, biome, guild, plot) %>%
+    dplyr::summarise(
+      prod_richness = mean(richness), 
+      prod_shannon = mean(shannon), 
+      prod_abundance = mean(abundance),
+      prod_cv = CV(abundance),
+      prod_stability = stability(abundance)) %>%
+    dplyr::left_join(cdr_biodiv_prod_synch, by = "plot")
+  
+  consumer_dss <- consumer_diversity %>%
+    dplyr::group_by(site, taxa_type, ecosystem, habitat_broad, habitat_fine, biome, guild, plot) %>%
+    dplyr::summarise(
+      con_richness = mean(richness), 
+      con_shannon = mean(shannon), 
+      con_abundance = mean(abundance),
+      con_cv = CV(abundance),
+      con_stability = stability(abundance)) %>%
+    dplyr::left_join(cdr_biodiv_con_synch, by = "plot")
+  
+  # calculate multitrophic stability
+  # combine producers and consumers
+  multitrophic_data <- merge(producer_data[,-which(names(producer_data) %in% c("guild", "taxa_type", "unit_abundance", "scale_abundance"))],
+                             consumer_data[,-which(names(consumer_data) %in% c("guild", "taxa_type", "unit_abundance", "scale_abundance"))],
+                             by = c("site", "ecosystem", "habitat_broad", "habitat_fine", "biome", "plot", "year"))
+  
+  # aggregate to calculate multritophic abundance and diversity
+  multitrophic_diversity <- 
+    data.frame(
+      site = multitrophic_data$site,
+      taxa_type = rep("multitroph", nrow(multitrophic_data)),
+      ecosystem = multitrophic_data$ecosystem,
+      habitat_broad = multitrophic_data$habitat_broad,
+      habitat_fine = multitrophic_data$habitat_fine,
+      biome = multitrophic_data$biome,
+      guild = rep("multitroph", nrow(multitrophic_data)),
+      plot = multitrophic_data$plot,
+      year = multitrophic_data$year,
+      richness = rowSums(multitrophic_data[, -which(names(multitrophic_data) %in% long_meta_cols)] > 0),
+      abundance = rowSums(multitrophic_data[, -which(names(multitrophic_data) %in% long_meta_cols)]),
+      shannon = vegan::diversity(multitrophic_data[, -which(names(multitrophic_data) %in% long_meta_cols)], "shannon")
+    )
+  
+  # calculate multitrophic diversity and stability
+  multitrophic_dss <- multitrophic_diversity %>%
+    dplyr::group_by(site, taxa_type, ecosystem, habitat_broad, habitat_fine, biome, guild, plot) %>%
+    dplyr::summarise(
+      multitroph_richness = mean(richness), 
+      multitroph_shannon = mean(shannon), 
+      multitroph_abundance = mean(abundance),
+      multitroph_cv = CV(abundance),
+      multitroph_stability = stability(abundance))
+  
+  # combine dfs for SEM
+  aggregate_dss <- ungroup(
+    left_join(producer_dss[,-which(names(producer_dss) %in% c("taxa_type", "guild"))],
+              consumer_dss[,-which(names(consumer_dss) %in% c("taxa_type", "guild"))], 
+              by = c("site", "plot", "habitat_broad", "habitat_fine", "biome", "ecosystem")) %>%
+      left_join(multitrophic_dss[,-which(names(multitrophic_dss) %in% c("taxa_type", "guild"))],
+                by = c("site", "plot", "habitat_broad", "habitat_fine", "biome", "ecosystem"))
+  )
+  
+  # create object names
+  producer_object_name <- paste0(ecosystem_type, "_producer_dss")
+  consumer_object_name <- paste0(ecosystem_type, "_consumer_dss")
+  multitrophic_object_name <- paste0(ecosystem_type, "_multitrophic_dss")
+  
+  # assign to global environment
+  assign(producer_object_name, producer_dss, envir = .GlobalEnv)
+  assign(consumer_object_name, consumer_dss, envir = .GlobalEnv)
+  assign(multitrophic_object_name, multitrophic_dss, envir = .GlobalEnv)
+  
+  if (write_csv) {
+    print("Writing to CSV...")
+    write_csv(producer_dss, paste0(producer_object_name, ".csv"))
+    write_csv(consumer_dss, paste0(consumer_object_name, ".csv"))
+    write_csv(multitrophic_dss, paste0(multitrophic_object_name, ".csv"))
+    
+  }
+}
+
 extract_ranges <- function(df, # a dataframe
                            group, # grouping variable (e.g. habitat)
                            columns # columns you want to summarize (e.g. richness, cover, synchrony)
