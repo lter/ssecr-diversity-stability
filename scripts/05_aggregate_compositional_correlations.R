4e735eec889d865567f3ab50db8df51947c5a64e
naltmank <nta8@gatech.edu>
2025-05-19 11:56:04 -0400

    Corrected axes to reflect z-standardized data

--- scripts/05_aggregate_compositional_correlations.R
+++ scripts/05_aggregate_compositional_correlations.R
@@ -1,32 +1,34 @@
 rm(list = ls())
 
 library(ggplot2)
-
+source(here::here("scripts", "00_functions.r"))
 # read data
 master <- read.csv(here::here(file = "data/synthesized_data", "master_stability.csv"))
 
 
-cor.test(log(master$prod_comp_stability+1), log(master$prod_stability+1), method = "kendall") # p-value = 3.921e-05, tau = -0.185
+cor.test(z_standard(master$prod_comp_stability),
+         z_standard(master$prod_stability), method = "kendall") # p-value = 3.921e-05, tau = 0.185
 
 (prod_stability_metric_corr <-
   ggplot() +
-  geom_point(data = master, aes(x = log(prod_comp_stability+1), y = log(prod_stability+1), colour = site, shape = ecosystem), size = 2.5) +
-  geom_smooth(data = master, aes(x = log(prod_comp_stability+1), y = log(prod_stability+1), colour = site), 
+  geom_point(data = master, aes(x = z_standard(prod_comp_stability), y = z_standard(prod_stability), colour = site, shape = ecosystem), size = 2.5) +
+  geom_smooth(data = master, aes(x = z_standard(prod_comp_stability), y = z_standard(prod_stability), colour = site), 
               method = lm, se = F) +
-  geom_smooth(data = master, aes(x = log(prod_comp_stability+1), y = log(prod_stability+1) ), 
+  geom_smooth(data = master, aes(x = z_standard(prod_comp_stability), y = z_standard(prod_stability) ), 
                 method = lm, se = T, colour = "black") +
   labs(x = "Compositional stability", y = "Aggregate stability", title = "a. Producer stability \n") +
   theme_classic() 
     
 )
 
-cor.test(log(master$con_comp_stability+1), log(master$con_stability+1), method = "kendall") # p-value = 7.184e-07, tau = -0.223
+
+cor.test(z_standard(master$con_comp_stability), z_standard(master$con_stability), method = "kendall") # p-value = 7.184e-07, tau = 0.223
 (con_stability_metric_corr <-
     ggplot() +
-    geom_point(data = master, aes(x = log(con_comp_stability+1), y = log(con_stability+1), colour = site, shape = ecosystem),  size = 2.5) +
-    geom_smooth(data = master, aes(x = log(con_comp_stability+1), y = log(con_stability+1), colour = site), 
+    geom_point(data = master, aes(x = z_standard(con_comp_stability), y = z_standard(con_stability), colour = site, shape = ecosystem), size = 2.5) +
+    geom_smooth(data = master, aes(x = z_standard(con_comp_stability), y = z_standard(con_stability), colour = site), 
                 method = lm, se = F) +
-    geom_smooth(data = master, aes(x = log(con_comp_stability+1), y = log(con_stability+1) ), 
+    geom_smooth(data = master, aes(x = z_standard(con_comp_stability), y = z_standard(con_stability) ), 
                 method = lm, se = T, colour = "black") +
     labs(x = "Compositional stability", y = "Aggregate stability", title = "b. Consumer stability \n") +
     theme_classic() 
