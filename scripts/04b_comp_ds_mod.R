8b0f88f43cbe70903f3fa42a3071f9769b016083
naltmank <nta8@gatech.edu>
2025-05-20 11:04:27 -0400

    updated to link data read to drive

--- scripts/04b_comp_ds_mod.R
+++ scripts/04b_comp_ds_mod.R
@@ -8,7 +8,11 @@ librarian::shelf(googledrive, vegan, lme4, car, dplyr, tidyr, lattice, lavaan,
 source(here::here("scripts", "00_functions.r"))
 
 #### MASTER ####
-master <- read.csv(here::here(file = "data/synthesized_data", "master_stability.csv"))
+drive_folder <- googledrive::drive_ls(googledrive::as_id("https://drive.google.com/drive/folders/1xa-ypKd_ovsRov_Ol_uvE7ZI2rCc5SSj"), type='csv')
+tmp <- tempfile(fileext = ".csv")
+download <- drive_download(drive_folder[drive_folder$name=="master_stability.csv",], path = tmp, overwrite = TRUE)
+master <- read.csv(tmp)
+
 model_stability(df = master,
                 ecosystem_type = "combined",
                 stability_metric = "compositional",
@@ -25,8 +29,8 @@ plot_combined_compositional_prod_richness_stability
 plot_combined_compositional_con_richness_stability
 # plot_combined_compositional_multi_richness_stability
 plot_combined_compositional_stability_richness_correlations
-write.csv(row.names = F, lmer_results_combined_compositional_richness,
-          here::here("data/summary_tables", "lmer_results_combined_compositional_richness.csv"))
+# write.csv(row.names = F, lmer_results_combined_compositional_richness,
+#           here::here("data/summary_tables", "lmer_results_combined_compositional_richness.csv"))
 
 ##### TERRESTRIAL ####
 terr <- subset(master, ecosystem == "terrestrial")
@@ -52,13 +56,13 @@ plot_terrestrial_compositional_stability_richness_correlations
 # view within-site coefficients and p-values (i.e. what results would be if modeled independently)
 # lm(producer_stability ~ producer_richness + consumer_richness)
 # View(site_results_terrestrial_prod_compositional_richness)
-write.csv(row.names = F, site_results_terrestrial_prod_compositional_richness,
-          here::here("data/summary_tables", "site_results_terrestrial_prod_compositional_richness.csv"))
+# write.csv(row.names = F, site_results_terrestrial_prod_compositional_richness,
+#           here::here("data/summary_tables", "site_results_terrestrial_prod_compositional_richness.csv"))
 
 # consumer_stability ~ producer_richness + consumer_richness
 # View(site_results_terrestrial_con_compositional_richness)
-write.csv(row.names = F, site_results_terrestrial_con_compositional_richness,
-          here::here("data/summary_tables", "site_results_terrestrial_con_compositional_richness.csv"))
+# write.csv(row.names = F, site_results_terrestrial_con_compositional_richness,
+#           here::here("data/summary_tables", "site_results_terrestrial_con_compositional_richness.csv"))
 
 # multitrophic_stability ~ producer_stability + consumer_stability
 # View(site_results_terrestrial_multi_compositional_richness)
@@ -69,8 +73,8 @@ write.csv(row.names = F, site_results_terrestrial_con_compositional_richness,
 # for producer and consumer stability, producer and consumer richness are the predictor variables
 # for multitrophic stability, producer and consumer stability are the predictor variables
 # View(lmer_results_terrestrial_compositional_richness)
-write.csv(row.names = F, lmer_results_terrestrial_compositional_richness,
-          here::here("data/summary_tables", "lmer_results_terrestrial_compositional_richness.csv"))
+# write.csv(row.names = F, lmer_results_terrestrial_compositional_richness,
+#           here::here("data/summary_tables", "lmer_results_terrestrial_compositional_richness.csv"))
 
 # view sem results
 sem_results_terrestrial_richness_compositional
@@ -100,13 +104,13 @@ plot_aquatic_compositional_stability_richness_correlations
 # view within-site coefficients and p-values (i.e. what results would be if modeled independently)
 # lm(producer_stability ~ producer_richness + consumer_richness)
 # View(site_results_aquatic_prod_compositional_richness)
-write.csv(row.names = F, site_results_aquatic_prod_compositional_richness,
-          here::here("data/summary_tables", "site_results_aquatic_prod_compositional_richness.csv"))
+# write.csv(row.names = F, site_results_aquatic_prod_compositional_richness,
+#           here::here("data/summary_tables", "site_results_aquatic_prod_compositional_richness.csv"))
 
 # consumer_stability ~ producer_richness + consumer_richness
 # View(site_results_aquatic_con_compositional_richness)
-write.csv(row.names = F, site_results_aquatic_con_compositional_richness,
-          here::here("data/summary_tables", "site_results_aquatic_con_compositional_richness.csv"))
+# write.csv(row.names = F, site_results_aquatic_con_compositional_richness,
+#           here::here("data/summary_tables", "site_results_aquatic_con_compositional_richness.csv"))
 
 # multitrophic_stability ~ producer_stability + consumer_stability
 # View(site_results_aquatic_multi_compositional_richness)
@@ -117,8 +121,8 @@ write.csv(row.names = F, site_results_aquatic_con_compositional_richness,
 # for producer and consumer stability, producer and consumer richness are the predictor variables
 # for multitrophic stability, producer and consumer stability are the predictor variables
 # View(lmer_results_aquatic_compositional_richness)
-write.csv(row.names = F, lmer_results_aquatic_compositional_richness,
-          here::here("data/summary_tables", "lmer_results_aquatic_compositional_richness.csv"))
+# write.csv(row.names = F, lmer_results_aquatic_compositional_richness,
+#           here::here("data/summary_tables", "lmer_results_aquatic_compositional_richness.csv"))
 
 # view sem results
 sem_results_aquatic_richness_compositional
