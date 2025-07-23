d429c76fe627633cbf0633bd457c6f3bf33538fa
naltmank <nta8@gatech.edu>
2025-07-23 16:19:10 -0400

    Tested component models with just z-standardized predictors. Created new preliminary plots. Tested new function and am still getting convergence errors despite component models running just fine.

--- scripts/wrangling_noam/noam_temp_terrestrial_dss.R
+++ scripts/wrangling_noam/noam_temp_terrestrial_dss.R
@@ -1,16 +1,201 @@
 rm(list = ls())
 librarian::shelf(googledrive, vegan, lme4, car, dplyr, tidyr, lattice, lavaan,
-                 piecewiseSEM, semPlot, ggplot2, codyn, ggpubr, performance, emmeans)
+                 piecewiseSEM, semPlot, ggplot2, codyn, ggpubr, performance, emmeans, DHARMa)
 
 
 
-# retrieve functions from 00_functions.r
+##### setup ####
 source(here::here("scripts", "00_functions.r"))
 
-# read temp terrestrial comb data
+###### read comb data #####
+comb <- read.csv(here::here(file = "data/synthesized_data", "combined_agg_stability.csv"))
 
-terr_comb <- read.csv(here::here(file = "data/synthesized_data", "terrestrial_agg_dss.csv"))
-# test function
+##### z-transform predictors #####
+comb <- comb %>%
+  group_by(site) %>%
+  mutate(
+    prod_richness_z = as.numeric(z_standard(prod_richness)),
+    con_richness_z = as.numeric(z_standard(con_richness)),
+    prod_shannon_z = as.numeric(z_standard(prod_shannon)),
+    con_shannon_z = as.numeric(z_standard(con_shannon))
+  ) %>%
+  ungroup()
+
+
+
+##### separate terrestrial and aquatic #####
+terr_comb <- subset(comb, ecosystem == "terrestrial")
+aquatic_comb <- subset(comb, ecosystem == "aquatic")
+
+
+
+#### test  component models ####
+##### terrestrial richness #####
+terr_prod_stab_mod <- lmer(prod_stability  ~ prod_richness_z + con_richness_z + (1|site),  data = terr_comb)
+summary(terr_prod_stab_mod)
+# Estimate Std. Error t value
+# (Intercept)       5.0179     1.4278   3.515
+# prod_richness_z  -0.8057     0.4479  -1.799
+# con_richness_z   -0.3351     0.4479  -0.748
+plot(DHARMa::simulateResiduals(terr_prod_stab_mod)) # all good
+Anova(terr_prod_stab_mod) 
+# prod_richness_z 0.07206 .
+# con_richness_z  0.45437   
+performance::r2(terr_prod_stab_mod) # 0.097, 0.643
+
+
+terr_con_stab_mod <- lmer(con_stability  ~ prod_richness_z + con_richness_z + (1|site),  data = terr_comb)
+summary(terr_con_stab_mod)
+# Estimate Std. Error t value
+# (Intercept)      1.70660    0.07397  23.072
+# prod_richness_z  0.10184    0.09009   1.130
+# con_richness_z  -0.04294    0.09009  -0.477
+plot(DHARMa::simulateResiduals(terr_con_stab_mod)) # all good
+Anova(terr_con_stab_mod) 
+# prod_richness_z  0.2583 
+# con_richness_z   0.6336  
+performance::r2(terr_con_stab_mod) # 0.041, 0.054
+
+##### terrestrial shannon #####
+terr_prod_stab_mod <- lmer(prod_stability  ~ prod_shannon_z + con_shannon_z + (1|site),  data = terr_comb)
+summary(terr_prod_stab_mod)
+# Estimate Std. Error t value
+# (Intercept)      5.0196     1.4279   3.515
+# prod_shannon_z  -0.5518     0.5416  -1.019
+# con_shannon_z   -0.4722     0.5416  -0.872
+plot(DHARMa::simulateResiduals(terr_prod_stab_mod)) # some quantile deviations
+plot(residuals(terr_prod_stab_mod), predict(terr_prod_stab_mod)) # possible negative trend
+Anova(terr_prod_stab_mod) 
+# Chisq Df Pr(>Chisq)
+# prod_shannon_z    0.3082
+# con_shannon_z     0.3832
+performance::r2(terr_prod_stab_mod) # 0.081, 0.625
+
+
+terr_con_stab_mod <- lmer(con_stability  ~ prod_shannon_z + con_shannon_z + (1|site),  data = terr_comb) 
+summary(terr_con_stab_mod)
+# Estimate Std. Error t value
+# (Intercept)     1.70534    0.07420  22.983
+# prod_shannon_z -0.03838    0.10274  -0.374
+# con_shannon_z   0.15289    0.10274   1.488
+plot(DHARMa::simulateResiduals(terr_con_stab_mod)) #  quantile deviations
+plot(residuals(terr_con_stab_mod), predict(terr_con_stab_mod)) # not terrible
+Anova(terr_con_stab_mod) 
+# prod_shannon_z  0.7087  
+# con_shannon_z   0.1367
+performance::r2(terr_con_stab_mod) # 0.095, 0.114
+
+##### aquatic richness #####
+aquatic_prod_stab_mod <- lmer(prod_stability  ~ prod_richness_z + con_richness_z + (1|site),  data = aquatic_comb)
+summary(aquatic_prod_stab_mod)
+# Estimate Std. Error t value
+# (Intercept)       6.8072     2.6614   2.558
+# prod_richness_z   1.6782     0.4196   4.000
+# con_richness_z   -0.2978     0.4196  -0.710
+plot(DHARMa::simulateResiduals(aquatic_prod_stab_mod)) # significant quantile deviations in simulated residuals, KS test sig
+plot(residuals(aquatic_prod_stab_mod), predict(aquatic_prod_stab_mod)) # lots of clumping from one outlying site
+Anova(aquatic_prod_stab_mod) 
+
+# prod_richness_z 6.347e-05 ***
+# con_richness_z  0.4779
+performance::r2(aquatic_prod_stab_mod) # 0.35, 0.592
+
+
+aquatic_con_stab_mod <- lmer(con_stability  ~ prod_richness_z + con_richness_z + (1|site),  data = aquatic_comb)
+summary(aquatic_con_stab_mod)
+# Estimate Std. Error t value
+# (Intercept)      1.76428    0.20681   8.531
+# prod_richness_z  0.11134    0.05697   1.954
+# con_richness_z   0.18344    0.05697   3.220
+plot(DHARMa::simulateResiduals(aquatic_con_stab_mod)) # same issues as prod stability
+plot(residuals(aquatic_con_stab_mod), predict(aquatic_con_stab_mod)) # possibly heteroskedastic
+Anova(aquatic_con_stab_mod) 
+# prod_richness_z  0.050649 .
+# con_richness_z  0.001281 **
+performance::r2(aquatic_con_stab_mod) # 0.068, 0.340
+
+##### aquatic shannon #####
+aquatic_prod_stab_mod <- lmer(prod_stability  ~ prod_shannon_z + con_shannon_z + (1|site),  data = aquatic_comb)
+summary(aquatic_prod_stab_mod)
+# Estimate Std. Error t value
+# (Intercept)      6.8115     2.6628   2.558
+# prod_shannon_z   0.1670     0.4151   0.402
+# con_shannon_z    0.6435     0.4151   1.550
+plot(DHARMa::simulateResiduals(aquatic_prod_stab_mod)) # similar issues
+plot(residuals(aquatic_prod_stab_mod), predict(aquatic_prod_stab_mod)) # same as richness
+Anova(aquatic_prod_stab_mod) 
+# Chisq Df Pr(>Chisq)
+# prod_shannon_z   0.6875
+# con_shannon_z    0.1211
+performance::r2(aquatic_prod_stab_mod) # 0.006, 0.562
+
+
+aquatic_con_stab_mod <- lmer(con_stability  ~ prod_shannon_z + con_shannon_z + (1|site),  data = aquatic_comb)
+summary(aquatic_con_stab_mod)
+# (Intercept)     1.76423    0.20682   8.530
+# prod_shannon_z  0.06723    0.05434   1.237
+# con_shannon_z   0.23499    0.05434   4.324
+plot(DHARMa::simulateResiduals(aquatic_con_stab_mod)) # still same issues 
+plot(residuals(aquatic_con_stab_mod), predict(aquatic_con_stab_mod)) # same as above
+Anova(aquatic_con_stab_mod) 
+# prod_shannon_z  0.216  
+# con_shannon_z   1.531e-05 ***
+performance::r2(aquatic_con_stab_mod) # 0.069, 0.341
+
+#### exploratory plots ####
+##### terrestrial richness #####
+(terr_prod_prod_plot <- 
+  ggplot() +
+  geom_point(data = terr_comb, aes(x = prod_richness_z, y = prod_stability , colour = site)) +
+  stat_smooth(data = terr_comb, aes(x =  prod_richness_z, y = prod_stability, colour = site),
+              method = "glm", se = F) +
+  stat_smooth(data = terr_comb, aes(x =  prod_richness_z, y = prod_stability),
+              method = "glm", se = T, colour = "black") +
+  theme_classic() 
+)
+
+(terr_prod_con_plot <- 
+    ggplot() +
+    geom_point(data = terr_comb, aes(x = con_richness_z, y = prod_stability , colour = site)) +
+    stat_smooth(data = terr_comb, aes(x =  con_richness_z, y = prod_stability, colour = site),
+                method = "glm", se = F) +
+    stat_smooth(data = terr_comb, aes(x =  con_richness_z, y = prod_stability),
+                method = "glm", se = T, colour = "black") +
+    theme_classic() 
+)
+
+(terr_prod_rich_stab_panel <- ggpubr::ggarrange(terr_prod_prod_plot, terr_prod_con_plot,
+                                     nrow = 1, ncol = 2)
+)
+# ggsave(plot = terr_prod_rich_stab_panel, filename =  here::here("figures/GLM", "terr_prod_stab_richness_z.png"), height = 7, width = 14)
+
+##### aquatic richness #####
+(aquatic_prod_prod_plot <- 
+    ggplot() +
+    geom_point(data = aquatic_comb, aes(x = prod_richness_z, y = prod_stability , colour = site)) +
+    stat_smooth(data = aquatic_comb, aes(x =  prod_richness_z, y = prod_stability, colour = site),
+                method = "glm", se = F) +
+    stat_smooth(data = aquatic_comb, aes(x =  prod_richness_z, y = prod_stability),
+                method = "glm", se = T, colour = "black") +
+    theme_classic() 
+)
+
+(aquatic_prod_con_plot <- 
+    ggplot() +
+    geom_point(data = aquatic_comb, aes(x = con_richness_z, y = prod_stability , colour = site)) +
+    stat_smooth(data = aquatic_comb, aes(x =  con_richness_z, y = prod_stability, colour = site),
+                method = "glm", se = F) +
+    stat_smooth(data = aquatic_comb, aes(x =  con_richness_z, y = prod_stability),
+                method = "glm", se = T, colour = "black") +
+    theme_classic() 
+)
+
+(aquatic_prod_rich_stab_panel <- ggpubr::ggarrange(aquatic_prod_prod_plot, aquatic_prod_con_plot,
+                                                nrow = 1, ncol = 2)
+)
+# ggsave(plot = aquatic_prod_rich_stab_panel, filename =  here::here("figures/GLM", "aquatic_prod_stab_richness_z.png"), height = 7, width = 14)
+
+#### Test function ####
 model_stability(df = terr_comb,
                 ecosystem_type = "terrestrial",
                 stability_metric = "aggregate",
@@ -19,294 +204,5 @@ model_stability(df = terr_comb,
                 con_diversity_col = "con_richness",
                 prod_stability_col = "prod_stability",
                 con_stability_col = "con_stability",
-                multi_stability_col = "multitroph_stability",
-                z_standard = FALSE)
-# terrestrial_richness_aggregate_sem_results
-### SCRATCH PAPER BELOW
-# create individual models for each site
-
-#### Producer stability ####
-# Initialize an empty dataframe to store results
-prod_stability_results <- data.frame(
-  site = character(),
-  intercept = numeric(),
-  prod_richness_coef = numeric(),
-  con_richness_coef = numeric(),
-  x2 = numeric(),
-  p_value = numeric(),
-  stringsAsFactors = FALSE
-)
-
-# Loop through each site
-for (s in unique(terr_comb$site)) {
-  # Subset data for the current site
-  site_data <- subset(terr_comb, site == s)
-  
-  # Fit linear model
-  model <- lm(prod_stability ~ prod_richness + con_richness, , data = site_data)
-  
-  # Extract coefficients
-  coefs <- coef(model)
-  
-  # Get ANOVA results
-  anova_results <- Anova(model) 
-  f_statistic <- anova_results$`LR Chisq`[2]  # Take F-statistic for the model
-  p_value <- anova_results$`Pr(>Chisq)`[2]        # Take p-value
-  
-  # Store results
-  prod_stability_results <- rbind(prod_stability_results, data.frame(
-    site = s,
-    intercept = unname(coefs["(Intercept)"]),
-    prod_shannon_coef = unname(coefs["prod_richness"]),
-    con_shannon_coef = unname(coefs["con_richness"]),
-    f_statistic = f_statistic,
-    p_value = p_value,
-    stringsAsFactors = FALSE
-  ))
-}
-
-
-
-# run as lmer with site as random effect
-comb_prod_mod <- lmer(prod_stability  ~ z_standard(prod_richness) + z_standard(con_richness) + (1|site),  data = terr_comb)
-summary(comb_prod_mod)
-Anova(comb_prod_mod) # producer X2 = 0.5, P = 0.47, consumer X2 = 14.7, P = 0.0001
-x <- Anova(comb_prod_mod)
-x$Chisq
-x$`Pr(>Chisq)`
-y <- performance::r2(comb_prod_mod) # 0.06, 0.79
-unname(y$R2_conditional)
-y$R2_marginal
-
-# basic plots
-(prod_stab_prod_div_plot <- 
-  ggplot() +
-  geom_point(data = terr_comb, aes(x = z_standard(prod_richness), y = prod_stability , colour = site)) +
-  stat_smooth(data = terr_comb, aes(x = z_standard(prod_richness), y = prod_stability, colour = site),
-              method = "glm", se = F) +
-  stat_smooth(data = terr_comb, aes(x = z_standard(prod_richness), y = prod_stability),
-              method = "glm", se = T, colour = "black") +
-  theme_classic() 
-)
-
-(prod_stab_con_div_plot <- 
-    ggplot() +
-    geom_point(data = terr_comb, aes(x = con_richness, y = log(prod_stability + 1), colour = site)) +
-    stat_smooth(data = terr_comb, aes(x = con_richness, y = log(prod_stability + 1), colour = site),
-                method = "lm", se = F) +
-    stat_smooth(data = terr_comb, aes(x = con_richness, y = log(prod_stability + 1)),
-                method = "lm", se = T, colour = "black") +
-    theme_classic() 
-)
-
-(prod_stab_panel <- ggpubr::ggarrange(prod_stab_prod_div_plot, prod_stab_con_div_plot,
-                                     nrow = 1, ncol = 2)
-)
-
-
-#### CONSUMER STABILITY ####
-# repeat the above but for consumer stability
-con_stability_results <- data.frame(
-  site = character(),
-  intercept = numeric(),
-  prod_richness_coef = numeric(),
-  con_richness_coef = numeric(),
-  x2 = numeric(),
-  p_value = numeric(),
-  stringsAsFactors = FALSE
-)
-
-# Loop through each site
-for (s in unique(terr_comb$site)) {
-  # Subset data for the current site
-  site_data <- subset(terr_comb, site == s)
-  
-  # Fit linear model
-  model <- lm(con_stability ~ prod_richness + con_richness, , data = site_data)
-  
-  # Extract coefficients
-  coefs <- coef(model)
-  
-  # Get ANOVA results
-  anova_results <- Anova(model) 
-  f_statistic <- anova_results$`LR Chisq`[2]  # Take F-statistic for the model
-  p_value <- anova_results$`Pr(>Chisq)`[2]        # Take p-value
-  
-  # Store results
-  con_stability_results <- rbind(con_stability_results, data.frame(
-    site = s,
-    intercept = unname(coefs["(Intercept)"]),
-    prod_shannon_coef = unname(coefs["prod_richness"]),
-    con_shannon_coef = unname(coefs["con_richness"]),
-    f_statistic = f_statistic,
-    p_value = p_value,
-    stringsAsFactors = FALSE
-  ))
-}
-
-
-
-# run as lmer with site as random effect
-comb_con_mod <- lmer(con_stability  ~ z_standard(prod_richness) + z_standard(con_richness) + (1|site), , data = terr_comb)
-summary(comb_con_mod)
-Anova(comb_con_mod) # prod richness X2 = 3,1, P = 0.08; con richness X2 = 003, P = 0.96
-performance::r2(comb_con_mod) # 0.05, 0.53
-
-# basic plots
-(con_stab_prod_div_plot <- 
-    ggplot() +
-    geom_point(data = terr_comb, aes(x = z_standard(prod_richness), y = con_stability, colour = site)) +
-    stat_smooth(data = terr_comb, aes(x = z_standard(prod_richness), y = con_stability, colour = site),
-                method = "lm", se = F) +
-    stat_smooth(data = terr_comb, aes(x = z_standard(prod_richness), y = con_stability),
-                method = "lm", se = T, colour = "black") +
-    theme_classic() 
-)
-
-(con_stab_con_div_plot <- 
-    ggplot() +
-    geom_point(data = terr_comb, aes(x = con_richness, y = con_stability, colour = site)) +
-    stat_smooth(data = terr_comb, aes(x = con_richness, y = con_stability, colour = site),
-                method = "glm", se = F) +
-    stat_smooth(data = terr_comb, aes(x = con_richness, y = con_stability),
-                method = "glm", se = T, colour = "black") +
-    theme_classic() 
-)
-
-(con_stab_panel <- ggpubr::ggarrange(con_stab_prod_div_plot, con_stab_con_div_plot,
-                                      nrow = 1, ncol = 2)
-)
-
-
-
-#### MULTITROPHIC STABILITY ####
-# repeat the above but for consumer stability
-multitrophic_stability_results <- data.frame(
-  site = character(),
-  intercept = numeric(),
-  prod_stability_coef = numeric(),
-  con_stability_coef = numeric(),
-  x2 = numeric(),
-  p_value = numeric(),
-  stringsAsFactors = FALSE
-)
-
-# Loop through each site
-for (s in unique(terr_comb$site)) {
-  # Subset data for the current site
-  site_data <- subset(terr_comb, site == s)
-  
-  # Fit linear model
-  model <- lm(multitroph_stability ~ prod_stability + con_stability, , data = site_data)
-  
-  # Extract coefficients
-  coefs <- coef(model)
-  
-  # Get ANOVA results
-  anova_results <- Anova(model) 
-  f_statistic <- anova_results$`LR Chisq`[2]  # Take F-statistic for the model
-  p_value <- anova_results$`Pr(>Chisq)`[2]        # Take p-value
-  
-  # Store results
-  multitrophic_stability_results <- rbind(multitrophic_stability_results, data.frame(
-    site = s,
-    intercept = unname(coefs["(Intercept)"]),
-    prod_stability_coef = unname(coefs["prod_richness"]),
-    con_stability_coef = unname(coefs["con_richness"]),
-    f_statistic = f_statistic,
-    p_value = p_value,
-    stringsAsFactors = FALSE
-  ))
-}
-
-
-
-# run as lmer with site as random effect
-comb_multitroph_mod <- lmer(multitroph_stability ~ prod_stability + con_stability + (1|site), , data = terr_comb)
-summary(comb_multitroph_mod)
-Anova(comb_multitroph_mod)
-performance::r2(comb_multitroph_mod) 
-
-# basic plots
-(multitroph_prod_stab_plot <- 
-    ggplot() +
-    geom_point(data = terr_comb, aes(x = prod_stability, y = multitroph_stability, colour = site)) +
-    stat_smooth(data = terr_comb, aes(x = prod_stability, y = multitroph_stability, colour = site),
-                method = "lm", se = F) +
-    stat_smooth(data = terr_comb, aes(x = prod_stability, y = multitroph_stability),
-                method = "lm", se = T, colour = "black") +
-    theme_classic() 
-)
-
-(multitroph_con_stab_plot <- 
-    ggplot() +
-    geom_point(data = terr_comb, aes(x = con_stability, y = multitroph_stability, colour = site)) +
-    stat_smooth(data = terr_comb, aes(x = con_stability, y = multitroph_stability, colour = site),
-                method = "glm", se = F) +
-    stat_smooth(data = terr_comb, aes(x = con_stability, y = multitroph_stability),
-                method = "glm", se = T, colour = "black") +
-    theme_classic() 
-)
-
-(con_stab_panel <- ggpubr::ggarrange(con_stab_prod_div_plot, con_stab_con_div_plot,
-                                     nrow = 1, ncol = 2)
-)
-
-
-
-#### Correlations ####
-# diversity correlations
-diversity_correlations <- terr_comb %>%
-  group_by(site) %>%
-  summarise(
-    kendall_tau = cor(prod_shannon, con_shannon, method = "kendall"),
-    p_value = cor.test(prod_shannon, con_shannon, method = "kendall")$p.value,
-    .groups = "drop"
-  )
-
-cor.test(terr_comb$prod_shannon, terr_comb$con_shannon, method = "kendall") # tau = 0.06, P = 0.22
-
-# stability correlations
-stability_correlations <- terr_comb %>%
-  group_by(site) %>%
-  summarise(
-    kendall_tau = cor(prod_stability, con_stability, method = "kendall"),
-    p_value = cor.test(prod_stability, con_stability, method = "kendall")$p.value,
-    .groups = "drop"
-  )
-
-cor.test(terr_comb$prod_stability, terr_comb$con_stability, method = "kendall") # tau = 0.08, P = 0.09
-
-# basic plots
-(con_prod_div_plot <- 
-    ggplot() +
-    geom_point(data = terr_comb, aes(x = prod_shannon, y = con_shannon, colour = site)) +
-    stat_smooth(data = terr_comb, aes(x = prod_shannon, y = con_shannon, colour = site),
-                method = "lm", se = F) +
-    stat_smooth(data = terr_comb, aes(x = prod_shannon, y = con_shannon),
-                method = "lm", se = T, colour = "black") +
-    theme_classic() 
-)
-
-
-(con_prod_stab_plot <- 
-    ggplot() +
-    geom_point(data = terr_comb, aes(x = prod_stability, y = con_stability, colour = site)) +
-    stat_smooth(data = terr_comb, aes(x = prod_stability, y = con_stability, colour = site),
-                method = "lm", se = F) +
-    stat_smooth(data = terr_comb, aes(x = prod_stability, y = con_stability),
-                method = "lm", se = T, colour = "black") +
-    theme_classic() 
-)
-
-(terr_corr_panel <- ggpubr::ggarrange(con_prod_div_plot, con_prod_stab_plot,
-                                      nrow = 1, ncol = 2)
-)
-
-modelList <- psem(comb_prod_mod, comb_con_mod, comb_multitroph_mod,
-                  prod_stability %~~% con_stability,
-                  prod_richness %~~% con_richness,
-                  terr_comb)
-z <- summary(modelList)
-z
-plot(modelList)
+                multi_stability_col = NULL,
+                transformation = "z")
