217e82ecb1a69968d67483a6fb64168187a148fb
naltmank <nta8@gatech.edu>
2025-08-06 13:48:06 -0400

    re-wrote to interface with drive. replaced old data with James's newly calculated bray curtis lengths

--- scripts/03b_combine_stability_results.R
+++ scripts/03b_combine_stability_results.R
@@ -4,133 +4,107 @@
 # Last Updated: May 13th, 2025
 
 rm(list = ls())
-librarian::shelf(tidyverse, here)
+librarian::shelf(tidyverse, googledrive, readr, tools)
 
 # read combined dss
 combined_agg_stability <- read.csv(here::here("data/synthesized_data", "combined_agg_stability.csv")) |> 
   filter(plot != "AHND")
 
-# Read length CSVs ----
-#### KNZ ####
-knz_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "knz_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
+# Read bray curtis producer csvs ----
+drive_auth()
+producer_folder_url <- "https://drive.google.com/drive/u/0/folders/1GHSn0Ffgn4PubbnzEn_vqcNEOejpEoXR"
+producer_folder_id <- gsub(".*/folders/([^/?]+).*", "\\1", producer_folder_url)
+
+# List CSV files in that folder
+producer_csv_files <- drive_ls(as_id(producer_folder_id))
+producer_csv_files <- producer_csv_files[grepl("\\.csv$", producer_csv_files$name), ]  # ensure only CSVs
+
+# skip usvi inverts because those are all benthic
+producer_skip_file <- "usvi_invert_producer_lengths.csv"
+
+# Loop through and assign each CSV to a dataframe
+for (i in seq_len(nrow(producer_csv_files))) {
+  original_name <- producer_csv_files$name[i]
+  # Skip USVI INVERTS
+  if (original_name == producer_skip_file) {
+    message("Skipping: ", original_name)
+    next
+  }
+  file_name <- file_path_sans_ext(original_name)
+  var_name <- make.names(file_name)
+  temp_path <- tempfile(fileext = ".csv")
   
-
-knz_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "knz_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-
-
-#### AIMS ####
-aims_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "aims_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
-
-aims_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "aims_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-
-#### KBS ####
-kbs_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "kbs_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
-
-kbs_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "kbs_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-
-#### CDR ####
-cdr_of_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "cdr_of_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
-cdr_of_producer_lengths$site <- rep("cdr_of", nrow(cdr_of_producer_lengths))
-
-cdr_of_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "cdr_of_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-
-#### USVI ####
-usvi_fish_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "usvi_fish_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
-
-usvi_fish_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "usvi_fish_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-
-# usvi_invert_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "usvi_invert_producer_lengths.csv")) %>%
-#   mutate(prod_comp_stability = 1/mean_length,
-#          prod_total_length = Trajectory) %>%
-#   select(site, plot, prod_comp_stability, prod_total_length)
-# 
-# usvi_invert_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "usvi_invert_consumer_lengths.csv")) %>%
-#   mutate(con_comp_stability = 1/mean_length,
-#          con_total_length = Trajectory) %>%
-#   select(site, plot, con_comp_stability, con_total_length)
+  drive_download(producer_csv_files[i, ], path = temp_path, overwrite = TRUE)
+  df <- read_csv(temp_path)
+  
+  # Apply mutation to all files
+  df <- df %>%
+    mutate(
+      prod_comp_stability = 1 / mean_segment_length,
+      prod_total_length = total_trajectory
+    ) %>%
+    select(site, plot, prod_comp_stability, prod_total_length)
+  
+  # Assign to environment
+  assign(var_name, df, envir = .GlobalEnv)
+}
 
 
-#### SBC ####
-sbc_fish_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "sbc_fish_producer_lengths.csv")) %>%
-   mutate(prod_comp_stability = 1/mean_length,
-          prod_total_length = Trajectory) %>%
-   select(site, plot, prod_comp_stability, prod_total_length)
-sbc_fish_producer_lengths$site <- rep("sbc_fish", nrow(sbc_fish_producer_lengths))
 
-sbc_fish_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "sbc_fish_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-sbc_fish_consumer_lengths$site <- rep("sbc_fish", nrow(sbc_fish_consumer_lengths))
+# Read bray curtis consumer csvs ----
+consumer_folder_url <- "https://drive.google.com/drive/u/0/folders/1_85P6tUSAB0MVGDYBmNJ7XUJnO6ZaPEM"
+consumer_folder_id <- gsub(".*/folders/([^/?]+).*", "\\1", consumer_folder_url)
 
-sbc_invert_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "sbc_invert_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
-sbc_invert_producer_lengths$site <- rep("sbc_invert", nrow(sbc_invert_producer_lengths))
+# List CSV files in that folder
+consumer_csv_files <- drive_ls(as_id(consumer_folder_id))
+consumer_csv_files <- consumer_csv_files[grepl("\\.csv$", consumer_csv_files$name), ]  # ensure only CSVs
 
-sbc_invert_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "sbc_invert_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-sbc_invert_consumer_lengths$site <- rep("sbc_invert", nrow(sbc_invert_consumer_lengths))
+# skip usvi inverts because those are all benthic
+consumer_skip_file <- "usvi_invert_consumer_lengths.csv"
 
-#### MCR ####
-mcr_fish_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "mcr_fish_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
-mcr_fish_producer_lengths$site <- rep("mcr_fish", nrow(mcr_fish_producer_lengths))
+# Loop through and assign each CSV to a dataframe
+for (i in seq_len(nrow(consumer_csv_files))) {
+  original_name <- consumer_csv_files$name[i]
+  # Skip USVI INVERTS
+  if (original_name == consumer_skip_file) {
+    message("Skipping: ", original_name)
+    next
+  }
+  file_name <- file_path_sans_ext(original_name)
+  var_name <- make.names(file_name)
+  temp_path <- tempfile(fileext = ".csv")
+  
+  drive_download(consumer_csv_files[i, ], path = temp_path, overwrite = TRUE)
+  df <- read_csv(temp_path)
+  
+  # Apply mutation to all files
+  df <- df %>%
+    mutate(
+      con_comp_stability = 1 / mean_segment_length,
+      con_total_length = total_trajectory
+    ) %>%
+    select(site, plot, con_comp_stability, con_total_length)
+  
+  # Assign to environment
+  assign(var_name, df, envir = .GlobalEnv)
+}
 
-mcr_fish_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "mcr_fish_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-mcr_fish_consumer_lengths$site <- rep("mcr_fish", nrow(mcr_fish_consumer_lengths))
 
-mcr_invert_producer_lengths <- read_csv(here("tables/wide_output_minimize/producer", "mcr_invert_producer_lengths.csv")) %>%
-  mutate(prod_comp_stability = 1/mean_length,
-         prod_total_length = Trajectory) %>%
-  select(site, plot, prod_comp_stability, prod_total_length)
-mcr_invert_producer_lengths$site <- rep("mcr_invert", nrow(mcr_invert_producer_lengths))
 
-mcr_invert_consumer_lengths <- read_csv(here("tables/wide_output_minimize/consumer", "mcr_invert_consumer_lengths.csv")) %>%
-  mutate(con_comp_stability = 1/mean_length,
-         con_total_length = Trajectory) %>%
-  select(site, plot, con_comp_stability, con_total_length)
-mcr_invert_consumer_lengths$site <- rep("mcr_invert", nrow(mcr_invert_consumer_lengths))
 
 #### MERGE WITH COMB STABILITY ####
+
+## REFACTOR CDR, SBC, MCR - needs to be done in the file - talk to james. Brute force here ##
+cdr_of_producer_lengths$site <- as.factor( rep("cdr_of", nrow(cdr_of_producer_lengths)) )
+sbc_fish_producer_lengths$site <- as.factor( rep("sbc_fish", nrow(sbc_fish_producer_lengths)) )
+sbc_fish_consumer_lengths$site <- as.factor( rep("sbc_fish", nrow(sbc_fish_consumer_lengths)) )
+sbc_invert_producer_lengths$site <- as.factor( rep("sbc_invert", nrow(sbc_invert_producer_lengths)) )
+sbc_invert_consumer_lengths$site <- as.factor( rep("sbc_invert", nrow(sbc_invert_consumer_lengths)) )
+mcr_fish_producer_lengths$site <- as.factor( rep("mcr_fish", nrow(mcr_fish_producer_lengths)) )
+mcr_fish_consumer_lengths$site <- as.factor( rep("mcr_fish", nrow(mcr_fish_consumer_lengths)) )
+mcr_invert_producer_lengths$site <- as.factor( rep("mcr_invert", nrow(mcr_invert_producer_lengths)) )
+mcr_invert_consumer_lengths$site <- as.factor( rep("mcr_invert", nrow(mcr_invert_consumer_lengths)) )
+
 
 ## Combine consumer lengths only
 all_consumer_lengths <- rbind(
