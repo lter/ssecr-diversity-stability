25e9a059d2ad4540c1b0395cb341eda8c2f3cacb
naltmank <nta8@gatech.edu>
2025-12-08 14:49:56 -0500

    updated diversity and stability calculations to incorporate herbivore datasets

--- scripts/02_diversity_agg_stability_minimize.R
+++ scripts/02_diversity_agg_stability_minimize.R
@@ -20,7 +20,12 @@ knz_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('knz_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 knz_con_wide <- read.csv(tmp)
 
-calculate_agg_stability(producer_data = knz_prod_wide, consumer_data = knz_con_wide, "knz")
+drive_download(drive_folder[grepl('knz_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+knz_herb_wide <- read.csv(tmp)
+
+
+calculate_agg_stability(producer_data = knz_prod_wide, consumer_data = knz_con_wide, herbivore_data = knz_herb_wide,
+                        "knz")
 
 ###### KBS ######
 drive_download(drive_folder[grepl('kbs_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -29,7 +34,11 @@ kbs_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('kbs_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 kbs_con_wide <- read.csv(tmp)
 
-calculate_agg_stability(producer_data = kbs_prod_wide, consumer_data = kbs_con_wide, "kbs")
+drive_download(drive_folder[grepl('kbs_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+kbs_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = kbs_prod_wide, consumer_data = kbs_con_wide, herbivore_data = kbs_herb_wide,
+                        "kbs")
 
 ###### CDR OLD FIELD ######
 drive_download(drive_folder[grepl('cdr_of_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -38,7 +47,11 @@ cdr_of_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('cdr_of_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 cdr_of_con_wide <- read.csv(tmp)
 
-calculate_agg_stability(producer_data = cdr_of_prod_wide, consumer_data = cdr_of_con_wide, "cdr_of")
+drive_download(drive_folder[grepl('cdr_of_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+cdr_of_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = cdr_of_prod_wide, consumer_data = cdr_of_con_wide, herbivore_data = cdr_of_herb_wide,
+                        "cdr_of")
 
 ###### JRN ######
 drive_download(drive_folder[grepl('jrn_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -47,7 +60,11 @@ jrn_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('jrn_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 jrn_con_wide <- read.csv(tmp)
 
-calculate_agg_stability(producer_data = jrn_prod_wide, consumer_data = jrn_con_wide, "jrn")
+drive_download(drive_folder[grepl('jrn_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+jrn_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = jrn_prod_wide, consumer_data = jrn_con_wide, herbivore_data = jrn_herb_wide,
+                        "jrn")
 
 ##### BEX #####
 drive_download(drive_folder[grepl('bex_he_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -56,11 +73,17 @@ bex_he_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('bex_he_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 bex_he_con_wide <- read.csv(tmp)
 
+drive_download(drive_folder[grepl('bex_he_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+bex_he_herb_wide <- read.csv(tmp)
+
 drive_download(drive_folder[grepl('bex_ae_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
 bex_ae_prod_wide <- read.csv(tmp)
 
 drive_download(drive_folder[grepl('bex_ae_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 bex_ae_con_wide <- read.csv(tmp)
+
+drive_download(drive_folder[grepl('bex_ae_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+bex_ae_herb_wide <- read.csv(tmp)
 
 drive_download(drive_folder[grepl('bex_se_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
 bex_se_prod_wide <- read.csv(tmp)
@@ -68,9 +91,15 @@ bex_se_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('bex_se_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 bex_se_con_wide <- read.csv(tmp)
 
-calculate_agg_stability(producer_data = bex_he_prod_wide, consumer_data = bex_he_con_wide, "bex_he")
-calculate_agg_stability(producer_data = bex_ae_prod_wide, consumer_data = bex_ae_con_wide, "bex_ae")
-calculate_agg_stability(producer_data = bex_se_prod_wide, consumer_data = bex_se_con_wide, "bex_se")
+drive_download(drive_folder[grepl('bex_se_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+bex_se_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = bex_he_prod_wide, consumer_data = bex_he_con_wide, herbivore_data = bex_he_herb_wide,
+                        "bex_he")
+calculate_agg_stability(producer_data = bex_ae_prod_wide, consumer_data = bex_ae_con_wide, herbivore_data = bex_ae_herb_wide,
+                        "bex_ae")
+calculate_agg_stability(producer_data = bex_se_prod_wide, consumer_data = bex_se_con_wide, herbivore_data = bex_se_herb_wide,
+                        "bex_se")
 
 # correct names
 knz_aggregate_dss <- knz_aggregate_dss %>% mutate(site_new = 'knz')
@@ -93,7 +122,7 @@ terrestrial_agg_stability <- rbind(
   bex_se_aggregate_dss
 )
 
-write.csv(row.names = F, terrestrial_agg_stability, 'data/synthesized_data/terrestrial_agg_dss_11052025.csv')
+write.csv(row.names = F, terrestrial_agg_stability, 'data/synthesized_data/terrestrial_agg_dss_12082025.csv')
 googledrive::drive_upload(media = file.path("data/synthesized_data/terrestrial_agg_dss_11052025.csv"), overwrite = T,
                           path = googledrive::as_id("https://drive.google.com/drive/folders/1xa-ypKd_ovsRov_Ol_uvE7ZI2rCc5SSj"))
 
@@ -107,7 +136,13 @@ gce_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('gce_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 gce_con_wide <- read.csv(tmp)
 #
-calculate_agg_stability(producer_data = gce_prod_wide, consumer_data = gce_con_wide, "gce")
+
+drive_download(drive_folder[grepl('gce_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+gce_herb_wide <- read.csv(tmp)
+
+
+calculate_agg_stability(producer_data = gce_prod_wide, consumer_data = gce_con_wide, herbivore_data = gce_herb_wide,
+                        "gce")
 
 ###### USVI ######
 # drive_download(drive_folder[grepl('usvi_invert_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -124,8 +159,11 @@ usvi_fish_prod_wide <- read.csv(tmp)
 #
 drive_download(drive_folder[grepl('usvi_fish_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 usvi_fish_con_wide <- read.csv(tmp)
+drive_download(drive_folder[grepl('usvi_fish_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+usvi_fish_herb_wide <- read.csv(tmp)
+calculate_agg_stability(producer_data = usvi_fish_prod_wide, consumer_data = usvi_fish_con_wide, herbivore_data = usvi_fish_herb_wide,
+                        "usvi_fish")
 
-calculate_agg_stability(producer_data = usvi_fish_prod_wide, consumer_data = usvi_fish_con_wide, "usvi_fish")
 
 ###### SBC ######
 drive_download(drive_folder[grepl('sbc_invert_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -133,8 +171,11 @@ sbc_invert_prod_wide <- read.csv(tmp)
 #
 drive_download(drive_folder[grepl('sbc_invert_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 sbc_invert_con_wide <- read.csv(tmp)
+drive_download(drive_folder[grepl('sbc_invert_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+sbc_invert_herb_wide <- read.csv(tmp)
+calculate_agg_stability(producer_data = sbc_invert_prod_wide, consumer_data = sbc_invert_con_wide, herbivore_data = sbc_invert_herb_wide,
+                        "sbc_invert")
 
-calculate_agg_stability(producer_data = sbc_invert_prod_wide, consumer_data = sbc_invert_con_wide, "sbc_invert")
 ####------
 
 drive_download(drive_folder[grepl('sbc_fish_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -142,8 +183,11 @@ sbc_fish_prod_wide <- read.csv(tmp)
 #
 drive_download(drive_folder[grepl('sbc_fish_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 sbc_fish_con_wide <- read.csv(tmp)
+drive_download(drive_folder[grepl('sbc_fish_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+sbc_fish_herb_wide <- read.csv(tmp)
+calculate_agg_stability(producer_data = sbc_fish_prod_wide, consumer_data = sbc_fish_con_wide, herbivore_data = sbc_fish_herb_wide,
+                        "sbc_fish")
 
-calculate_agg_stability(producer_data = sbc_fish_prod_wide, consumer_data = sbc_fish_con_wide, "sbc_fish")
 
 ###### MCR ######
 drive_download(drive_folder[grepl('mcr_invert_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -151,8 +195,12 @@ mcr_invert_prod_wide <- read.csv(tmp)
 #
 drive_download(drive_folder[grepl('mcr_invert_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 mcr_invert_con_wide <- read.csv(tmp)
+drive_download(drive_folder[grepl('mcr_invert_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+mcr_invert_herb_wide <- read.csv(tmp)
 
-calculate_agg_stability(producer_data = mcr_invert_prod_wide, consumer_data = mcr_invert_con_wide, "mcr_invert")
+calculate_agg_stability(producer_data = mcr_invert_prod_wide, consumer_data = mcr_invert_con_wide, herbivore_data = mcr_invert_herb_wide,
+                        "mcr_invert")
+
 ####------
 
 drive_download(drive_folder[grepl('mcr_fish_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -161,7 +209,11 @@ mcr_fish_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('mcr_fish_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 mcr_fish_con_wide <- read.csv(tmp)
 
-calculate_agg_stability(producer_data = mcr_fish_prod_wide, consumer_data = mcr_fish_con_wide, "mcr_fish")
+drive_download(drive_folder[grepl('mcr_fish_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+mcr_fish_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = mcr_fish_prod_wide, consumer_data = mcr_fish_con_wide, herbivore_data = mcr_fish_herb_wide,
+                        "mcr_fish")
 
 ###### AIMS ######
 drive_download(drive_folder[grepl('aims_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -170,7 +222,12 @@ aims_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('aims_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 aims_con_wide <- read.csv(tmp)
 #
-calculate_agg_stability(producer_data = aims_prod_wide, consumer_data = aims_con_wide, "aims")
+drive_download(drive_folder[grepl('aims_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+aims_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = aims_prod_wide, consumer_data = aims_con_wide, herbivore_data = aims_herb_wide,
+                        "aims")
+
 
 ###### NTL_TROUT ######
 drive_download(drive_folder[grepl('ntl_trout_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -179,7 +236,12 @@ ntl_trout_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('ntl_trout_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 ntl_trout_con_wide <- read.csv(tmp)
 #
-calculate_agg_stability(producer_data = ntl_trout_prod_wide, consumer_data = ntl_trout_con_wide, "ntl_trout")
+drive_download(drive_folder[grepl('ntl_trout_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+ntl_trout_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = ntl_trout_prod_wide, consumer_data = ntl_trout_con_wide, herbivore_data = ntl_trout_herb_wide,
+                        "ntl_trout")
+
 
 ###### NTL_MADISON ######
 drive_download(drive_folder[grepl('ntl_madison_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -188,7 +250,13 @@ ntl_madison_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('ntl_madison_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 ntl_madison_con_wide <- read.csv(tmp)
 #
-calculate_agg_stability(producer_data = ntl_madison_prod_wide, consumer_data = ntl_madison_con_wide, "ntl_madison")
+
+drive_download(drive_folder[grepl('ntl_madison_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+ntl_madison_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = ntl_madison_prod_wide, consumer_data = ntl_madison_con_wide, herbivore_data = ntl_madison_herb_wide,
+                        "ntl_madison")
+
 
 ###### ADK ######
 drive_download(drive_folder[grepl('adk_producers', drive_folder$name),], path = tmp, overwrite = TRUE)
@@ -197,7 +265,12 @@ adk_prod_wide <- read.csv(tmp)
 drive_download(drive_folder[grepl('adk_consumers', drive_folder$name),], path = tmp, overwrite = TRUE)
 adk_con_wide <- read.csv(tmp)
 #
-calculate_agg_stability(producer_data = adk_prod_wide, consumer_data = adk_con_wide, "adk")
+
+drive_download(drive_folder[grepl('adk_herbivore', drive_folder$name),], path = tmp, overwrite = TRUE)
+adk_herb_wide <- read.csv(tmp)
+
+calculate_agg_stability(producer_data = adk_prod_wide, consumer_data = adk_con_wide, herbivore_data = adk_herb_wide,
+                        "adk")
 
 ###### COMBINE MARINE AGGREGATE DFS ######
 # add one more column, because some marine sites have two sets of producer-consumer data
@@ -213,7 +286,7 @@ ntl_madison_aggregate_dss <- ntl_madison_aggregate_dss %>% mutate(site_new = 'nt
 adk_aggregate_dss <- adk_aggregate_dss %>% mutate(site_new = 'adk')
 
 marine_agg_stability <- rbind(
-#  gce_aggregate_dss,
+  gce_aggregate_dss,
   usvi_fish_aggregate_dss,
   sbc_invert_aggregate_dss,
   sbc_fish_aggregate_dss,
@@ -225,13 +298,13 @@ marine_agg_stability <- rbind(
   adk_aggregate_dss
 )
 
-write.csv(row.names = F, marine_agg_stability, 'data/synthesized_data/marine_agg_dss_10282025.csv')
+write.csv(row.names = F, marine_agg_stability, 'data/synthesized_data/marine_agg_dss_12082025.csv')
 googledrive::drive_upload(media = file.path("data/synthesized_data/marine_agg_dss_10282025.csv"), overwrite = T,
                           path = googledrive::as_id("https://drive.google.com/drive/folders/1xa-ypKd_ovsRov_Ol_uvE7ZI2rCc5SSj"))
 
 combined_agg_stability <- rbind(terrestrial_agg_stability, marine_agg_stability)
-write.csv(row.names = F, combined_agg_stability, 'data/synthesized_data/combined_agg_dss_11052025.csv')
-googledrive::drive_upload(media = file.path("data/synthesized_data/combined_agg_dss_11052025.csv"), overwrite = T,
+write.csv(row.names = F, combined_agg_stability, 'data/synthesized_data/combined_agg_dss_12082025.csv')
+googledrive::drive_upload(media = file.path("data/synthesized_data/combined_agg_dss_12082025.csv"), overwrite = T,
                           path = googledrive::as_id("https://drive.google.com/drive/folders/1xa-ypKd_ovsRov_Ol_uvE7ZI2rCc5SSj"))
 
 
